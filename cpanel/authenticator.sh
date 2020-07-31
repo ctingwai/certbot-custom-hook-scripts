208c8cc0dbbca107ee1f9d111aac20f0aa72fdf8
Ting Wai <tingwai@skynorthdigital.com>
2020-07-31 17:39:26 +0800

    made curl silence

--- cpanel/authenticator.sh
+++ cpanel/authenticator.sh
@@ -4,7 +4,7 @@ source ./config.sh
 
 # Create TXT record
 echo "Creating TXT zone record for $CREATE_DOMAIN on domain ${DOMAIN}"
-result=$(curl -H"Authorization: cpanel $USERNAME:$API_KEY" "https://$CPANEL_DOMAIN:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=ZoneEdit&cpanel_jsonapi_func=add_zone_record&domain=$DOMAIN&name=$CREATE_DOMAIN&type=TXT&txtdata=$CERTBOT_VALIDATION")
+result=$(curl -s -H"Authorization: cpanel $USERNAME:$API_KEY" "https://$CPANEL_DOMAIN:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=ZoneEdit&cpanel_jsonapi_func=add_zone_record&domain=$DOMAIN&name=$CREATE_DOMAIN&type=TXT&txtdata=$CERTBOT_VALIDATION")
 if [[ "$(echo $result | grep -o '"status":[01]' | cut -d':' -f2)" = '0' ]]; then
     echo 'Failed to create TXT record...'
     exit 1
--- cpanel/cleanup.sh
+++ cpanel/cleanup.sh
@@ -4,12 +4,12 @@ source ./config.sh
 echo "Removing certbot validation records for $CERTBOT_DOMAIN"
 
 # Find records for cleanup
-records="$(curl -H"Authorization: cpanel ${USERNAME}:${API_KEY}" "https://${CPANEL_DOMAIN}:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=ZoneEdit&cpanel_jsonapi_func=fetchzone_records&domain=${DOMAIN}&customonly=1&name=${CREATE_DOMAIN}&type=TXT")"
+records="$(curl -s -H"Authorization: cpanel ${USERNAME}:${API_KEY}" "https://${CPANEL_DOMAIN}:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=ZoneEdit&cpanel_jsonapi_func=fetchzone_records&domain=${DOMAIN}&customonly=1&name=${CREATE_DOMAIN}&type=TXT")"
 
 # Remove records recursively if exists
 for line in $(echo $records | grep -o '"line":[0-9]\{1,2\}' | cut -d':' -f2); do
     echo "Removing TXT record for $CERTBOT_DOMAIN on line $line"
-    result=$(curl -H"Authorization: cpanel ${USERNAME}:${API_KEY}" "https://${DOMAIN}:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=ZoneEdit&cpanel_jsonapi_func=remove_zone_record&domain=${DOMAIN}&line=${line}" | grep -o '"status":[01]' | cut -d':' -f2)
+    result=$(curl -s -H"Authorization: cpanel ${USERNAME}:${API_KEY}" "https://${DOMAIN}:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=ZoneEdit&cpanel_jsonapi_func=remove_zone_record&domain=${DOMAIN}&line=${line}" | grep -o '"status":[01]' | cut -d':' -f2)
     if [[ "$result" = '0' ]]; then
         echo 'Failed to remove records, please remove it manually before continue'
         exit 1
